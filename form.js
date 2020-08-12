aa5dbfdfda2d65f98a854c59584d343172e3ae5f
Parrot Live user <user@localhost.localdomain>
2020-08-12 16:29:50 +0000

    adding

--- form.js
+++ form.js
@@ -1,7 +1,19 @@
 $(document).ready(function(){
-    $("#userfname").hide()
-    $("#userlname").hide()
-    $("#useremail").hide()
-    $("#userpass").hide()
-    $("#usercpass").hide()
+    $("#userfname").hide();
+    $("#userlname").hide();
+    $("#useremail").hide();
+    $("#userpass").hide();
+    $("#usercpass").hide();
+
+    var email_err=true;
+    var pass_err=true;
+    var cpass_err=true;
+
+    $("#user").key(function(){
+        user_check()
+    });
+    function user_check(){
+        var user_name=$("#user").val();
+        alert(user_name)
+    }
 })
