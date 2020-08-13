1d622b96acdc1d9b88bc51f796c22c180a91e981
Parrot Live user <user@localhost.localdomain>
2020-08-13 07:35:23 +0000

    Form Validation

--- form.js
+++ form.js
@@ -1,19 +1,11 @@
 $(document).ready(function(){
-    $("#userfname").hide();
-    $("#userlname").hide();
-    $("#useremail").hide();
-    $("#userpass").hide();
-    $("#usercpass").hide();
 
-    var email_err=true;
-    var pass_err=true;
-    var cpass_err=true;
-
-    $("#user").keyup(function(){
-        user_check();
-    });
-    function user_check(){
-        var user_name=$("#user").val();
-        alert(user_name);
-    };
-});
+    $("#formjs").validate({
+        rules:{
+            fname:{
+                required:true,
+                minlength:4
+            }
+        }
+    })
+})
