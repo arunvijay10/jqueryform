5cea72b147be5c858ce928616c532ca1725493bc
Parrot Live user <user@localhost.localdomain>
2020-08-13 08:20:47 +0000

    Form Validation

--- form.js
+++ form.js
@@ -15,7 +15,15 @@ $(document).ready(function(){
                 required:true,
                 email:true
 
+            },
+            pass:{
+                minLength: 12, 
+                minUpperCase: 2, 
+                minLowerCase: 2, 
+                minDigits: 2,  
+                minSpecial: 2,      
             }
+
           
         }
     })
